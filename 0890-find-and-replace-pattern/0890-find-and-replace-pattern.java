5a74c669979e0fe4158723afba1557ac11061103
VTU29015 <vtu29015@veltech.edu.in>
2026-09-09 23:32:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (90.94%) - LeetHub

---
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            if (matches(word, pattern)) {
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean matches(String word, String pattern) {
+        char[] map1 = new char[26];
+        char[] map2 = new char[26];
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char p = pattern.charAt(i);
+            char w = word.charAt(i);
+
+            // Pattern character already mapped
+            if (map1[p - 'a'] != '\0' && map1[p - 'a'] != w) {
+                return false;
+            }
+
+            // Word character already mapped to another pattern character
+            if (map2[w - 'a'] != '\0' && map2[w - 'a'] != p) {
+                return false;
+            }
+
+            map1[p - 'a'] = w;
+            map2[w - 'a'] = p;
+        }
+
+        return true;
+    }
+}
